59dab3eb6d87f54dcf39622f297207a50bb82120
tom-sapletta-com <tom@sapletta.com>
2022-07-28 21:55:13 +0200

    new functions and tests

---
+++ prependToSentence.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+INPUT=
+if [ ! -t 0 ]; then
+  IFS='' read -d '' -r INPUT
+fi
+SEPARATOR=""
+if [ ! -z "$1" ]; then
+  params=("$@")
+  for i in "${params[@]}"; do
+    echo -n "$i$SEPARATOR"
+  done
+fi
+[ ! -z "$INPUT" ] && echo -n "$INPUT"
+echo ""
